e30c0aec727a49f39d3a4deb814c2974c8cde745
Klus3kk <lukasz.bielaszewski@onet.pl>
2024-12-09 20:14:52 +0100

    Implemented basic structure of the Flask app.

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
 """Flask App (/update for taking data u(t), updating the simulation, /plot for data to plot)"""
+from flask import Flask, requests, jsonify 
+from modules.simulation import calculate_heat_loss_glass, calculate_heat_loss_walls, calculate_next_temperature
+import requests as request
+
+app = Flask(__name__)
+
